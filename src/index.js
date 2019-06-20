f4d6523bbceec053a8aa09cccb2c91a95e928a94
Symt <28537235+Symt@users.noreply.github.com>
2019-06-20 10:30:48 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import XYFrameWrapper from './XYFrameWrapper';
+
+ReactDOM.render(<XYFrameWrapper />, document.getElementById('root'));
